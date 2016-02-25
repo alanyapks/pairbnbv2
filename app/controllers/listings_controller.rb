c87158b0071a2def9051011126f1545613e04172
Next <nextacademy@email.com>
2016-02-25 15:29:10 +0800

    Add create listing form but haven finish

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,35 @@
+class ListingsController < ApplicationController
+  def index
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_user.listings.build(listing_params)
+    if @listing.save
+      flash[:success] = "Created new listing"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:property_type, :room_type, :accommodates, :bedrooms, :beds, :bathrooms, :name, :description, :country, :address, :city, :state, :zip_code)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :listings, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
