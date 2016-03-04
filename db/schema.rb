b28bb17e1d18276092e631b4fb6af155ae01628e
Next <nextacademy@email.com>
2016-03-04 12:59:31 +0800

    Add photos to listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304023143) do
+ActiveRecord::Schema.define(version: 20160304044903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160304023143) do
     t.string   "zip_code"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.json     "photos"
   end
 
   create_table "users", force: :cascade do |t|
