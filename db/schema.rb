74fd1e7cdb18931ed78315bf66cf551741d4b783
Next <nextacademy@email.com>
2016-03-04 10:54:31 +0800

    Add details to user database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301070152) do
+ActiveRecord::Schema.define(version: 20160304023143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 20160301070152) do
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
     t.string   "image"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
